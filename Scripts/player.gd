04652df7a833ae264981fba40d9b2fe8d8ef78e2
its.ssskye <its.ssskyler@gmail.com>
2026-02-01 10:06:14 +0700

    Fix game errors and files

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,22 +1,22 @@
 extends RigidBody2D
 
 var jump_velocity = -600
-var stability: int = 100
 var is_on_ground
 
 @onready var ground_detector = $"Ground Detector"
-@onready var stability_bar = $"Camera/Stability Bar"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	# Jump
 	if Input.is_action_just_pressed("space_bar") and is_on_ground:
 		apply_central_impulse(Vector2(0, jump_velocity))
-		stability -= 40
+
+	# Rotate
+	var rotation_dir = Input.get_action_strength("left") - Input.get_action_strength("right")
 
 	# Detect if player is and isn't on ground
 	if ground_detector.is_colliding():
@@ -24,18 +24,16 @@ func _process(_delta: float) -> void:
 	else:
 		is_on_ground = false
 
-	# Lose and regain stability
-	if abs(linear_velocity.x) > 850 and is_on_ground:
-		stability -= 3
-	elif abs(linear_velocity.x) < 850 and is_on_ground:
-		stability += 2
+	if rotation_degrees >= 45 and rotation_degrees <= 90:
+		mass += 0.5 * delta
+	elif not rotation_degrees >= 45 and rotation_degrees <= 90:
+		mass = 1
 
-	# Quit the game if player has no stability
-	if stability == 0:
-		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
+func _on_wall_colliders_area_entered(_area: Area2D) -> void:
+	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
 
-	# Clamp stability to not go over limits
-	stability = clamp(stability, 0, 100)
 
-	# Update stability bar based on player's stability
-	stability_bar.value = stability
+func _on_hit_collider_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Enemy"):
+		area.queue_free()
+		linear_velocity += Vector2(50, 0)
