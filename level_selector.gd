e975f0fa0dc12447762e43ed3e7fe8569a71b0c6
J. Pickles <noahjstringer@gmail.com>
2026-02-04 11:01:57 -0500

    Changes to level selector
    Made level selector set current level to level that player is in, and made mouse hide when beginning levels

---
+++ level_selector.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+# Play level 1
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	LevelData.current_level = ("res://scenes/level_1.tscn")
+
+
+# Play level 2
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	LevelData.current_level = ("res://scenes/level_2.tscn")
+
+
+# Play level 3
+func _on_level_3_pressed() -> void:
+	pass # Replace with function body.
