c45e1aa79e98f0278217d6ed2c3733d01fcf6afa
J. Pickles <noahjstringer@gmail.com>
2026-02-06 11:38:00 -0500

    Added game over to missing a button press

---
+++ player.gd
@@ -0,0 +1,115 @@
+extends RigidBody2D
+
+var jump_velocity = -600
+var is_on_ground
+var can_finish_lvl = false
+var pressed_button
+var interactable: Area2D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	# Dive
+	if Input.is_action_pressed("dive") and not is_on_ground:
+		linear_velocity.y += 12
+
+	# Exit level
+	if Input.is_action_just_pressed("exit"):
+		get_tree().paused = true
+		var go_scene = load("res://scenes/pause_menu.tscn").instantiate()
+		get_tree().root.add_child(go_scene)
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	# Kill enemy
+	if not interactable == null and Input.is_action_just_pressed("interact") and interactable.is_in_group("Enemy"):
+		interactable.queue_free()
+		LevelData.enemies_killed += 1
+
+	# Interact with button
+	if not interactable == null and Input.is_action_just_pressed("interact") and interactable.is_in_group("Button"):
+		interactable.activate()
+		pressed_button = true
+		LevelData.buttons_activated += 1
+	elif not interactable == null and not pressed_button:
+		pressed_button = false
+
+	# Get player's rotation input
+	var rotation_dir = Input.get_axis("left", "right")
+
+	# Rotate sled when player is in air and gives input
+	if not is_on_ground and rotation_dir:
+		angular_velocity += rotation_dir * delta * 3.5
+	elif is_on_ground:
+		pass
+
+	# Detect ground collision with GroundDetector1
+	if $GroundDetector1.is_colliding():
+		is_on_ground = true
+	elif not $GroundDetector2.is_colliding():
+		is_on_ground = false
+
+	# Detect ground collision with GroundDetector2
+	if $GroundDetector2.is_colliding():
+		is_on_ground = true
+	elif not $GroundDetector1.is_colliding():
+		is_on_ground = false
+
+	if LevelData.buttons_activated == 3:
+		can_finish_lvl = true
+	elif not LevelData.buttons_activated == 3:
+		can_finish_lvl = false
+
+# Detect if player lands on their head
+func _on_head_detection_area_entered(area: Area2D) -> void:
+# Player dies if they land on their head
+	if area.is_in_group("Ice"):
+		get_tree().paused = true
+		var go_scene = load("res://scenes/game_over.tscn").instantiate()
+		get_tree().root.add_child(go_scene)
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+# Detect if player is not on ground
+func _on_ground_detection_area_exited(area: Area2D) -> void:
+	if area.is_in_group("Ice"):
+		is_on_ground = false
+
+# Interaction entrance detection
+func _on_interact_detection_area_entered(area: Area2D) -> void:
+	interactable = area
+	pressed_button = false
+
+	# If player enters a tutorial trigger, slow down time
+	if area.is_in_group("Tutorial Trigger"):
+		Engine.time_scale = 0.1
+
+	if area.is_in_group("Deathzone"):
+		get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	# Enter safehouse if player is able to
+	if area.is_in_group("Safehouse") and can_finish_lvl:
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/main_menu.tscn")
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif area.is_in_group("Safehouse") and not can_finish_lvl:
+		get_tree().paused = true
+		var go_scene = load("res://scenes/game_over.tscn").instantiate()
+		get_tree().root.add_child(go_scene)
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+# Interaction exiting detection
+func _on_interact_detection_area_exited(area: Area2D) -> void:
+	interactable = null
+
+	# If player leaves a tutorial trigger, reset time speed
+	if area.is_in_group("Tutorial Trigger"):
+		Engine.time_scale = 1
+
+	if area.is_in_group("Button") and not pressed_button:
+		get_tree().paused = true
+		var go_scene = load("res://scenes/game_over.tscn").instantiate()
+		get_tree().root.add_child(go_scene)
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
