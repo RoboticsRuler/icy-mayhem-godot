62d9ba75089a9f8dd973889c244f6ea7e8616aba
J. Pickles <noahjstringer@gmail.com>
2026-02-07 14:40:17 -0500

    Starting next level and returning to menu now resets your counters instead of keeping their values

--- you_win.gd
+++ you_win.gd
@@ -19,9 +19,13 @@ func _on_next_pressed() -> void:
 	# If there is another level, then go to the next one
 	if not LevelData.next_level == "none":
 		load_level(str(LevelData.next_level))
+		LevelData.buttons_activated = 0
+		LevelData.enemies_killed = 0
 	# If there is not another level, then go to the main menu
 	elif LevelData.next_level == "none":
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+		LevelData.buttons_activated = 0
+		LevelData.enemies_killed = 0
 
 
 func _on_exit_pressed() -> void:
