679fed517daa842c3444f67e20e4873f363f8765
its.ssskye <its.ssskyler@gmail.com>
2026-02-07 21:38:01 +0700

    Update game over script

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -8,7 +8,6 @@ func _on_restart_pressed() -> void:
 	get_tree().reload_current_scene()
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	queue_free()
-	LevelData.buttons_activated -= 3
 	
 func _on_exit_pressed() -> void:
 	get_tree().paused = false
