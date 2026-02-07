6364ddecb7e159829b5a4fbba63a5d859d0129f3
J. Pickles <noahjstringer@gmail.com>
2026-02-07 13:45:32 -0500

    Fixed "Next" code to go to the correct level using "Next Level" variable

---
+++ you_win.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func load_level(level_path: String) -> void:
+	LevelData.current_level = level_path
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	get_tree().change_scene_to_file(level_path)
+
+func _on_next_pressed() -> void:
+	# If there is another level, then go to the next one
+	if not LevelData.next_level == "none":
+		load_level(LevelData.next_lvl)
+	# If there is not another level, then go to the main menu
+	elif LevelData.next_level == "none":
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
